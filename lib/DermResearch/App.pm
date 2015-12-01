50ad0aa04203430772dccf45127ed44adba0619c
BryantDavidDavis <davis.bryant@gmail.com>
2015-12-01 01:10:27 -0800

    adds handlebars updates foundation and updates site

--- lib/DermResearch/App.pm
+++ lib/DermResearch/App.pm
@@ -1,6 +1,7 @@
 package DermResearch::App;
 use Dancer2;
 
+
 our $VERSION = '0.1';
 
 get '/' => sub {
@@ -13,11 +14,28 @@ get '/contact' => sub {
 
 prefix '/experience' => sub {
 	get '/studies' => sub {
-		template 'experience/studies';
+		my @header_inserts = ('/derm-research-studies.json');
+		my @footer = ('/javascripts/handlebars-v4.0.4.js');
+		my @footer_inserts = (
+			'experience/studies/footer-inserts.tt'
+		);
+		my @handlebars_templates = (
+			'experience/studies/studies-accordion.handlebars'
+		);
+		template 'experience/studies' => {
+			header_inserts => \@header_inserts,
+			footer => \@footer,
+			handlebars_templates => \@handlebars_templates,
+			footer_inserts => \@footer_inserts
+		};
 	};
 
 	get '/companies' => sub {
 		template 'experience/companies';
+	};
+
+	get '/facilities' => sub {
+		template 'experience/facilities';
 	};
 };
 
